e053d321c696dccdc74c8f445d809590b631b4fc
Mikhail <futurer@inbox.ru>
2023-12-12 14:29:37 +0300

    Create IsPalindromeString.java

---
+++ IsPalindromeString.java
@@ -0,0 +1,17 @@
+public class IsPalindromeString {
+  
+    public static void main(String[] args) {
+      
+        String str = "1aoiuytyuioa1";
+
+        boolean isPolindrome = true;
+        for (var i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPolindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPolindrome);
+    }
+}
