7b060074ebd3131169a331fa3f156e868c90515d
Mikhail <futurer@inbox.ru>
2024-01-24 00:02:53 +0300

    Update ReverseArray.java

--- ReverseArray.java
+++ ReverseArray.java
@@ -10,15 +10,23 @@ public class ReverseArray {
             arr[i] = i;
         }
 
+        String[] arr1 = new String[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr1[i] = i + "str";
+        }
+
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
         reverse(arr);
+        reverse(arr1);
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
     }
 
-    public static void reverse(Integer[] arr) {
+    public static <T> void reverse(T[] arr) {
         for (int i = 0; i < arr.length / 2; i++) {
 
-            Integer tmp = arr[i];
+            T tmp = arr[i];
             arr[i] = arr[arr.length - i - 1];
             arr[arr.length - i - 1] = tmp;
         }
