bbacedd6109db617092de84e1f3f94114cdbf3f2
Mikhail <futurer@inbox.ru>
2024-01-25 23:56:21 +0300

    Update MaxLengthSubstring.java

--- MaxLengthSubstring.java
+++ MaxLengthSubstring.java
@@ -2,5 +2,17 @@
  * Вычислить максимальную длину подстроки из неповторяющихся символов
  */
 public class SubstringMaxLength {
-    String str = "aahdgssjjdkkewlleekvjjff888wjks";
+    String str = new String("aahdgssjjdkkewlleekvjjff888wjks");
+
+    LinkedHashSet<Character> charSequence = new LinkedHashSet<>();
+    
+    for (int i = 0; i < str.length(); i++) {
+        if (charSequence.contains(str.charAt(i))) {
+            charSequence.clear();
+        }
+        charSequence.add(str.charAt(i));
+    }
+
+    System.out.printf("str max substring length: %d%n", charSequence.size());
+    charSequence.forEach(System.out::print);
 }
