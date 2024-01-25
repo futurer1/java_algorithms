07a7b88605cb9cd6e1fd77edd491c81465efb81e
Mikhail <futurer@inbox.ru>
2024-01-26 00:29:31 +0300

    Update MaxLengthSubstring.java

--- MaxLengthSubstring.java
+++ MaxLengthSubstring.java
@@ -1,18 +1,58 @@
+package java_algorithms;
+
 /**
  * Вычислить максимальную длину подстроки из неповторяющихся символов
  */
 public class SubstringMaxLength {
-    String str = new String("aahdgssjjdkkewlleekvjjff888wjks");
 
-    LinkedHashSet<Character> charSequence = new LinkedHashSet<>();
-    
-    for (int i = 0; i < str.length(); i++) {
-        if (charSequence.contains(str.charAt(i))) {
-            charSequence.clear();
-        }
-        charSequence.add(str.charAt(i));
+    public static void main(String[] args) {
+        String str = new String("aahhd8wjksgssjjdkkewlleekvjjff88");
+        String str1 = new String("aahhnnffkkdddkeell");
+        String str2 = new String("1234567890");
+
+        System.out.printf("str max substring length: %d%n", getMaxLengthUniqueSubstring(str).length());
+        System.out.println(getMaxLengthUniqueSubstring(str));
+        // str max substring length: 8
+        // hd8wjksg
+
+        System.out.printf("str1 max substring length: %d%n", getMaxLengthUniqueSubstring(str1).length());
+        System.out.println(getMaxLengthUniqueSubstring(str1));
+        // str1 max substring length: 3
+        // dke
+
+        System.out.printf("str2 max substring length: %d%n", getMaxLengthUniqueSubstring(str2).length());
+        System.out.println(getMaxLengthUniqueSubstring(str2));
+        // str2 max substring length: 1
+        // 1
     }
 
-    System.out.printf("str max substring length: %d%n", charSequence.size());
-    charSequence.forEach(System.out::print);
+    private static String getMaxLengthUniqueSubstring(String str) {
+        LinkedHashSet<Character> curCharSequence = new LinkedHashSet<>();
+        LinkedHashSet<Character> maxCharSequence = new LinkedHashSet<>();
+
+        Character lastChar;
+
+        if (!str.isEmpty()) {
+            maxCharSequence.add(str.charAt(0));
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            lastChar = str.charAt(i);
+
+            if (curCharSequence.contains(str.charAt(i))) {
+
+                if (maxCharSequence.size() < curCharSequence.size()) {
+                    maxCharSequence.clear();
+                    maxCharSequence.addAll(curCharSequence);
+                }
+                curCharSequence.clear();
+                curCharSequence.add(lastChar);
+            }
+            curCharSequence.add(str.charAt(i));
+        }
+        
+        return maxCharSequence.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
 }
